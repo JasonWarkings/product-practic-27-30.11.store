1cfa273e04caa5a610dc8ff70285c638660d041d
Одорский Доминик <dominik10072007@gmail.com>
2024-11-27 10:32:07 +0500

    feat: add layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,94 @@
 </script>
 
 <template>
-<h1>Main page</h1>
+  <div class="row mt-4">
+    <div class="col-md-5">
+      <select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">Vegetables</option>
+  <option value="2">Fruits</option>
+  <option value="3">Berries</option>
+      </select>
+    </div>
+      <div class="col-md-5">
+            <input type="email" class="form-control"  placeholder="shary be name">
+         </div>
+      <div class="col-md-2">
+        <button type="button" class="btn btn-outline-warning" disabled>Warning</button>  
+    </div>
+  </div>
+<div class="container"> 
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://main-cdn.sbermegamarket.ru/big1/hlr-system/428/425/752/325/184/4/100028179877b0.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Banana</h5>
+        <p class="card-text">$15</p>
+        <p class="card-text">Fruits</p>
+        <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://avatars.mds.yandex.net/i?id=bfd8e81c92de32b4107489679b86794be2515e2c-5599693-images-thumbs&n=13" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Apple</h5>
+        <p class="card-text">$10</p>
+        <p class="card-text">Fruits</p>
+        <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://klike.net/uploads/posts/2023-02/1677474656_3-26.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Marakuya</h5>
+        <p class="card-text">$20</p>
+        <p class="card-text">Fruits</p>
+        <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://avatars.mds.yandex.net/i?id=ea05b7ce20ad3224766ed644617641100b907c14-4509882-images-thumbs&n=13" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Tomato</h5>
+        <p class="card-text">$8</p>
+        <p class="card-text">Vegatables</p>
+        <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+<div class="col">
+    <div class="card h-100">
+      <img src="https://www.ekologicznemieso.com.pl/winogrono-jasne.2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Grappy</h5>
+        <p class="card-text">$15</p>
+        <p class="card-text">Berrys</p>
+        <div class="card-footer text-end">
+          <button type="button" class="btn btn-outline-info">Detail</button>
+          <button type="button" class="btn btn-outline-success">Cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 </template>
