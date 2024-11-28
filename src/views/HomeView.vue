e924cab6d7a1156a584161b14dfae4e5d7f8d4a0
Одорский Доминик <dominik10072007@gmail.com>
2024-11-28 13:19:59 +0500

    feat: add site functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,67 @@
 <script setup>
 import { useCategoryStore } from '@/stores/category';
 import { useProductStore } from '@/stores/product';
-import { ref,} from 'vue'
+import { useCartStore } from '@/stores/cart';
+import { ref, } from 'vue';
 
-const productsStore =useProductStore();
-const categoryStore =useCategoryStore();
+const productStore = useProductStore ();
+const categoryStore = useCategoryStore();
+const cartStore = useCartStore();
 const category = ref(null);
-const searchText = ref("");
+const searchText = ref('');
 const products = ref([]);
-products.value = productsStore.products;
+products.value = productStore.products; 
+
+
 const filterCategory = () => {
-  products.value = productsStore.filterProductsByCategoryName(category.value, null);
+products.value = productStore.productsFilteredByCategoryName(category.value, null);
+searchText.value = '';
 }
-const searchName = () => {
+const SearchName = () => {
+products.value = productStore.productsFilteredByCategoryName(null, searchText.value );
+category.value = '';
+}
+
+const resetAll = () => {
+  searchText.value = '';
   category.value = null;
-  products.value = productsStore.filterProductsByCategoryName(null, searchText.value);
+  products.value = productStore.products; 
 }
 </script>
 
 <template>
-<div class="container">
-  <div class="row mt-4">
-    <div class="col md-5">
-      <select class="form-select" aria-label="Default select example" v-model="category" @change=filterCategory >
-  <option selected >Select category</option>
-  <option v-for="category in categoryStore.categories"
-  :value="category"
-  :key="category"
-  >{{ category }}
-</option>
-
-</select>
-</div>
+  <div class="container">
+    <div class="row mt-4 my-3">
       <div class="col md-5">
-        <input type="text" class="form-control" value="Search by name" aria-label="readonly input example" v-model="searchText" @input="searchName">
+        <select class="form-select" aria-label="Default select example" v-model="category" @change="filterCategory">
+          <option selected :value="null">Select category</option>
+          <option v-for="category in categoryStore.categories" :value="category" :key="category">{{ category }}</option>
+        </select>
       </div>
-        <div class="col md-2">
-          <button type="button" class="btn btn-outline-warning">reset</button>
-    
-     
+      <div class="col md-5">
+        <input type="text" @input="searchName" class="form-control" aria-label="readonly input example" v-model="searchText">
+      </div>
+      <div class="col md-2">
+        <button type="button" class="btn btn-outline-warning" @click="resetFilters">Reset</button>
+      </div>
     </div>
+
     <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col" v-for="product in products" :key="product.id">
-    <div class="card">
-      <img :src="product.img" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">{{ product.name }}</h5>
-        <p class="card-text">${{ product.price }}</p>
-        <p class="card-text">{{ product.category }}</p>
-        <p class="card-text">Date:{{ product.publish_at }}</p>
-        
-        <div class="card-footer text-end">
-          <button type="button" class="btn btn-outline-info">Detail</button>
-          <button type="button" class="btn btn-outline-success">Cart</button>
+      <div class="col" v-for="product in products" :key="product.id">
+        <div class="card">
+          <img :src="product.img" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">${{ product.price }}</p>
+            <p class="card-text">{{ product.category }}</p>
+            <p class="card-text">Date:{{ product.publish_at }}</p>
+            <div class="card-footer text-end">
+              <router-link :to="`/product/${product.id}`" class="btn btn-outline-info me-2">Detail</router-link>
+              <button type="button" @click="cartStore.addToCart(product.id)" class="btn btn-outline-success">Cart</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
   </div>
-  
-</div>
-  </div>
-</div>
 </template>
